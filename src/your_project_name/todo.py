7831f35bddab33937a5b116106b9e668362dc5c0
Olof Harrysson <harrysson.olof@gmail.com>
2025-02-18 13:39:09 +0100

    add json

--- src/your_project_name/todo.py
+++ src/your_project_name/todo.py
@@ -1,6 +1,18 @@
+import dotenv
+import jsonargparse
+
+
 def main() -> None:
     print("Hello World!")
 
 
+def parse_args() -> jsonargparse.Namespace:
+    parser = jsonargparse.ArgumentParser()
+    parser.add_function_arguments(main)
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
-    main()
+    dotenv.load_dotenv(".env")
+    args = parse_args()
+    main(**args)
