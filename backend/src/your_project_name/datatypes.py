605dbef2972325531147694969acd4771a8af4a6
Olof Harrysson <harrysson.olof@gmail.com>
2025-04-17 21:59:26 +0200

    add logger and datatypes

---
+++ backend/src/your_project_name/datatypes.py
@@ -0,0 +1,10 @@
+from joblib import Memory
+from pathlib import Path
+
+project_python_root = Path(__file__).parents[2]
+err_msg = f"{project_python_root.name=} not in [{project_python_root.name}]"
+assert project_python_root.name in ["backend"], err_msg
+CACHE_DIR = f"{project_python_root}/data/cache"
+
+Path(CACHE_DIR).mkdir(parents=True, exist_ok=True)
+MEMORY = Memory(location=CACHE_DIR, verbose=0)
---
+++ backend/src/your_project_name/utils/logger.py
@@ -0,0 +1,19 @@
+import logging
+
+logger = logging.getLogger("atworld-logger")
+
+# Create and configure handler
+handler = logging.StreamHandler()
+handler.setFormatter(
+    logging.Formatter(
+        fmt="%(asctime)s - %(name)s - %(levelname)s - %(filename)s:%(lineno)d - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+)
+
+# Configure only our logger instance
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
+
+# Prevent propagation to root logger
+logger.propagate = False
