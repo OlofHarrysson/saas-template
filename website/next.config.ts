7ab6cd5aa934179af4a58f3639345298c29fe87f
Olof Harrysson <harrysson.olof@gmail.com>
2025-06-07 19:19:58 +0200

    appwrites not building on vercel

--- website/next.config.ts
+++ website/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ["node-appwrite"],
+  },
 };
 
 export default nextConfig;
